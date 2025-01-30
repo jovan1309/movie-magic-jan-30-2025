cd4c9863e4f9bd7a36fdee22aa958fe07b39bc10
pepi1401 <pptarnovo1401@gmail.com>
2025-01-30 22:17:51 +0100

    Install and setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('IT WORKS');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('It Works');
+});
+
+app.listen(5001, () => console.log('Server is listening on http://localhost:5001...'));
